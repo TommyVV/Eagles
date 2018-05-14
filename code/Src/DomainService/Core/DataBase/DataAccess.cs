f32f3ae5e70bb553aaf1ca444513b687201fd4b1
TommyVV <644598674@qq.com>
2018-05-14 21:06:24 +0800

    Reconstruction code

---
+++ code/Src/DomainService/Core/DataBase/DataAccess.cs
@@ -0,0 +1,12 @@
+﻿using Eagles.Interface.Core.DataBase;
+
+namespace Eagles.DomainService.Core.DataBase
+{
+    public class DataAccess:IDataAccess
+    {
+        public void GetAreas(string id)
+        {
+            // call db  do some thing 
+        }
+    }
+}
--- code/Src/DomainService/Core/Test/TestHandler.cs
+++ code/Src/DomainService/Core/Test/TestHandler.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using System.Xml.Serialization;
 using Eagles.Application.Model.Test;
 using Eagles.Base.Configuration;
+using Eagles.Interface.Core.DataBase;
 using Eagles.Interface.Core.Test;
-using Eagles.Interface.Infrastructure.DataAccess;
-using DBModel= Ealges.Infrastructure.DataBaseModel.Model;
 
 namespace Eagles.DomainService.Core.Test
 {
     public class TestHandler: ITestHandler
     {
-        private readonly IAreaDataAccess areaData;
+        private readonly IDataAccess dataAccess;
 
         private readonly IConfigurationManager configurationManager;
 
-        public TestHandler(IAreaDataAccess areaData, IConfigurationManager configurationManager)
+        public TestHandler(IDataAccess dataAccess, IConfigurationManager configurationManager)
         {
-            this.areaData = areaData;
+            this.dataAccess = dataAccess;
             this.configurationManager = configurationManager;
         }
 
         public TestResponse Porcess(TestRequest request)
         {
             var s=configurationManager.GetConfiguration<TestConfig>();
-            var areaInfo = areaData.GetAreas(request.Id);
+            dataAccess.GetAreas(request.Id);
             var result = new TestResponse()
             {
                 AreaInfo = new List<AreaInfo>()
@@ -34,17 +33,7 @@ namespace Eagles.DomainService.Core.Test
                 DateTime = DateTime.Now,
                 TestNode = s.TestNode
             };
-            areaInfo.ForEach(x => result.AreaInfo.Add(ConvertAreaInfo(x)));
             return result;
-        }
-
-        private AreaInfo ConvertAreaInfo(DBModel.Area areaInfo)
-        {
-            return new AreaInfo
-            {
-                AreaId = areaInfo.AreaId,
-                AreaName =areaInfo.AreaName
-            };
         }
     }
 
