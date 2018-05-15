b844c339aad1f95b0bc33b94aeb319ad27c5acf7
晨皓 孙 <baby_sun001@hotmail.com>
2018-05-15 21:40:45 +0800

    修改接口，不抱错

---
+++ code/Src/Infrastructure/DataAccess/PartyMissionDapper.cs
@@ -0,0 +1,32 @@
+﻿using Eagles.Interface.Infrastructure.DataAccess;
+using Ealges.Infrastructure.DataBaseModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eagles.Infrastructure.DataAccess
+{
+    class PartyMissionDapper : IPartyMissionAccess
+    {
+        private List<PartyMission> _list = new List<PartyMission>
+        {
+            new PartyMission()
+            {
+                PmId = 1,
+                PmName = "第一次全国人民代表大会"
+            },
+            new PartyMission()
+            {
+                PmId = 2,
+                PmName = "第二次全国人民代表大会"
+            }
+        };
+
+        public PartyMission GetPmInfo(int id)
+        {
+            return _list.Find(x => x.PmId == id);
+        }
+    }
+}
