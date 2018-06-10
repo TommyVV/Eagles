67c62766079883cb82bd14824fd96af0a9706f96
TommyVV <644598674@qq.com>
2018-06-10 21:31:16 +0800

    add db commad

---
+++ code/Src/Base/DataBase/Implement/DbManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using Eagles.Base.Logger;
+using MySql.Data.MySqlClient;
+
+namespace Eagles.Base.DataBase.Implement
+{
+    public class DbManager: IDbManager
+    {
+        private readonly ILogger logger;
+
+        private const string connstr =
+            "server=120.77.46.183;Port=3306;User Id=appuser;password=appuser@123;Database=Eagles";
+
+        public DbManager(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public List<T> Query<T>(string command, object parameter)
+        {
+            var conn=new MySqlConnection(connstr);
+            var result=new List<T>();
+            try
+            {
+                result=conn.Query<T>(command, parameter).ToList();
+            }
+            catch (Exception e)
+            {
+                logger.LoggerError(e.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return result;
+        }
+
+        public int Excuted(string command, object paramster)
+        {
+            var conn = new MySqlConnection(connstr);
+            int result = 0;
+            try
+            {
+                 result=conn.Execute(command,paramster);
+            }
+            catch (Exception e)
+            {
+                logger.LoggerError(e.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return result;
+        }
+    }
+}
