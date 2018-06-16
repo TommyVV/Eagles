4a3514bccd856ae72696f77f6a34ffe4d2ef8b83
TommyVV <644598674@qq.com>
2018-06-16 17:53:29 +0800

    update base db manager

--- code/Src/Base/DataBase/IDBManager.cs
+++ code/Src/Base/DataBase/IDBManager.cs
@@ -4,8 +4,8 @@ namespace Eagles.Base.DataBase
 {
     public interface IDbManager:IInterfaceBase
     {
-        List<T> Query<T>(string command, object parameter);
+        List<T> Query<T>(string command, object parameter=null);
 
-        int Excuted(string command, object paramster);
+        int Excuted(string command, object paramster=null);
     }
 }
