9f0cdd5151151e926d31f886120628243087615d
晨皓 孙 <baby_sun001@hotmail.com>
2018-06-22 20:51:54 +0800

    新闻详情查询修复bug

--- code/Src/DomainService/DataAccess/NewsDA/NewsDA.cs
+++ code/Src/DomainService/DataAccess/NewsDA/NewsDA.cs
@@ -18,14 +18,13 @@ namespace Ealges.DomianService.DataAccess.NewsDA
         public List<TbNews> GetModuleNews(int moduleId,int appId,int count)
         {
             return dbManager.Query<TbNews>(@"SELECT OrgId,NewsId,ShortDesc,Title,HtmlContent,Author,Source,Module,Status,BeginTime,EndTime,TestId,Attach1,Attach2,Attach3,Attach4,Attach5,
-OperId,CreateTime,IsImage,IsVideo,IsAttach,IsClass,IsLearning,IsText,ViewCount,ReviewId,CanStudy,ImageUrl FROM eagles.tb_news where Module=@Module And OrgId=@OrgId limit @Count",new {Module = moduleId, OrgId = appId, Count = count});
-
+OperId,CreateTime,IsImage,IsVideo,IsAttach,IsClass,IsLearning,IsText,ViewCount,ReviewId,CanStudy,ImageUrl from eagles.tb_news where Module=@Module And OrgId=@OrgId limit @Count",new {Module = moduleId, OrgId = appId, Count = count});
         }
 
         public TbNews GetNewsDetail(int newsId, int appId)
         {
             return dbManager.QuerySingle<TbNews>(@"SELECT OrgId,NewsId,ShortDesc,Title,HtmlContent,Author,Source,Module,Status,BeginTime,EndTime,TestId,Attach1,Attach2,Attach3,Attach4,Attach5,
-OperId,CreateTime,IsImage,IsVideo,IsAttach,IsClass,IsLearning,IsText,ViewCount,ReviewId,CanStudy,ImageUrlFROM eagles.tb_news where NewsId=@NewsId And OrgId=@OrgId", new { NewsId = newsId, OrgId = appId });
+OperId,CreateTime,IsImage,IsVideo,IsAttach,IsClass,IsLearning,IsText,ViewCount,ReviewId,CanStudy,ImageUrl from eagles.tb_news where NewsId=@NewsId And OrgId=@OrgId", new { NewsId = newsId, OrgId = appId });
         }
 
         public List<TbQuestEx> GetNewsTest(int testId)
