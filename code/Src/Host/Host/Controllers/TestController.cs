c423c765b6f682dd3c4ce4e5b523ef96a7a90304
TommyVV <644598674@qq.com>
2018-06-24 22:02:02 +0800

    add md5 encrypt

---
+++ code/Src/Host/Host/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Eagles.Base.Md5Helper;
+
+namespace Eagles.Application.Host.Controllers
+{
+    public class TestController: ApiController
+    {
+        private readonly IMd5Helper md5Helper;
+
+
+        public TestController(IMd5Helper md5Helper)
+        {
+            this.md5Helper = md5Helper;
+        }
+
+        [HttpGet]
+        public string Test(string str)
+        {
+            return md5Helper.Md5Encypt(str);
+        }
+    }
+}
