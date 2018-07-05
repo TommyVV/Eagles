f84a3865d5be441958cc87481bfa5fc87bb1b266
TommyVV <644598674@qq.com>
2018-07-05 22:52:27 +0800

    add cache

---
+++ Management/src/Base/Cache/Implement/CacheHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+
+namespace Eagles.Base.Cache.Implement
+{
+    public class CacheHelper:ICacheHelper
+    {
+        public void SetData<T>(string key, T data)
+        {
+            HttpRuntime.Cache.Insert(key,data);
+        }
+
+        public T GetData<T>(string key) where T : class
+        {
+            var data = HttpRuntime.Cache.Get(key);
+
+            return data as T;
+        }
+    }
+}
