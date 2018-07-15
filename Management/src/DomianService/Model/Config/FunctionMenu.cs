a9ccd959ff3dc2d4f7efcda7542a9b38a53cc3e3
TommyVV <644598674@qq.com>
2018-07-15 11:48:47 +0800

    add cache

---
+++ Management/src/DomianService/Model/Config/FunctionMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using Eagles.Base.Configuration;
+
+namespace Eagles.DomainService.Model.Config
+{
+    [XmlRoot("Menus")]
+    [XmlPath("/Configuration/FunctionMenu.xml")]
+    public class FunctionMenu
+    {
+        public List<Node> Nodes { get; set; }
+    }
+
+   
+    public class Node
+    {
+        //[XmlAttribute("Priority")]
+        //public string Priority { get; set; }
+
+        //[XmlAttribute("Name")]
+        //public string Name { get; set; }
+
+        [XmlElement("Menu")]
+        public List<Menu> Menus { get; set; }
+    }
+
+    public class Menu
+    {
+        [XmlAttribute("Priority")]
+        public string Priority { get; set; }
+
+        [XmlAttribute("Name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("FunCode")]
+        public string FunCode { get; set; }
+    }
+
+}
