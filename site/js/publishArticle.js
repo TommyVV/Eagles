f57ce3d1164aee738dd45f234e4ea3e5bb9d4cdc
qixiaoer <1120637164@qq.com>
2018-07-16 14:45:03 +0800

    样式

--- site/js/publishArticle.js
+++ site/js/publishArticle.js
@@ -11,7 +11,7 @@ $("#name").html(name);
 $('.publish-btn').on('click', function(e) {
 	e.preventDefault();
 	var title = $('.publish-title').val(); //标题
-	var type = $('.publish-type').val(); //文章类型
+	var type = $('.selectpicker').val(); //文章类型
 	var content = $('.publish-content').val(); //文章内容
 	if(!title) {
 		alert('请填写文章标题');
@@ -37,13 +37,13 @@ $('.publish-btn').on('click', function(e) {
 		success: function(res) {
 			var data = res.Result;
 			if(res.Code == 00) {
-				window.location.href = 'myArticle.html?NewsType=' + $('.publish-type').val() + '';
+				window.location.href = 'myArticle.html?NewsType=' + $('.selectpicker').val() + '';
 				//文章发布成功
 			}
 		}
 	})
 })
-$(".publish-type").change(function() {
+$(".selectpicker").change(function() {
 	if($(this).val() == 3) {
 		$('.assign').show();
 	} else {
@@ -53,7 +53,7 @@ $(".publish-type").change(function() {
  $("#subord").click(function() {
         window.location.href = "subordinateList.html?appId=" + appId + "&type=0";
     });
-branchUsers(token, appId); //党员支部信息展示
+/*branchUsers(token, appId); //党员支部信息展示
 function branchUsers(token, appId) {
 	$.ajax({
 		type: "post",
@@ -77,4 +77,4 @@ function branchUsers(token, appId) {
 			}
 		}
 	})
-}
+}*/
