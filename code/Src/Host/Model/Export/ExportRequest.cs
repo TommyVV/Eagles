ce50cdc8691e26a2af67f1df2ef54857216fda40
Admin <Vesoft@DESKTOP-L9UH261>
2018-07-16 17:21:02 +0800

    导出代码提交

---
+++ code/Src/Host/Model/Export/ExportRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Eagles.Application.Model.Export
+{
+    /// <summary>
+    /// 导出
+    /// </summary>
+    public class ExportRequest
+    {
+        /// <summary>
+        /// sql
+        /// </summary>
+        public string ExportSql { get; set; }
+
+        /// <summary>
+        /// 列名
+        /// </summary>
+        public List<string> ColumnName { get; set; }
+    }
+}
---
+++ code/Src/Host/Model/Export/ExportResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Eagles.Application.Model.Export
+{
+    /// <summary>
+    /// 导出
+    /// </summary>
+    public class ExportResponse
+    {
+        /// <summary>
+        /// 生成路径
+        /// </summary>
+        public string FileUrl { get; set; }
+    }
+}
