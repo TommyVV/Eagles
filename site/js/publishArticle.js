b4261240e0e92c6a0ce21b51b13fd6c34c85e017
qixiaoer <1120637164@qq.com>
2018-07-16 18:55:40 +0800

    1

--- site/js/publishArticle.js
+++ site/js/publishArticle.js
@@ -11,7 +11,7 @@ $("#name").html(name);
 $('.publish-btn').on('click', function(e) {
 	e.preventDefault();
 	var title = $('.publish-title').val(); //标题
-	var type = $('.selectpicker').val(); //文章类型
+	var type = $('#selectpicker').val(); //文章类型
 	var content = $('.publish-content').val(); //文章内容
 	if(!title) {
 		bootoast({
@@ -52,23 +52,33 @@ $('.publish-btn').on('click', function(e) {
 		success: function(res) {
 			var data = res.Result;
 			if(res.Code == 00) {
-				window.location.href = 'myArticle.html?NewsType=' + $('.selectpicker').val() + '';
+				window.location.href = 'myArticle.html?NewsType=' + $('#selectpicker').val() + '';
 				//文章发布成功
 			}
 		}
 	})
 })
-$(".selectpicker").change(function() {
+$("#selectpicker").change(function() {
 	if($(this).val() == 3) {
 		$('.assign').show();
 	} else {
 		$('.assign').hide();
 	}
 });
- $("#subord").click(function() {
-        window.location.href = "subordinateList.html?appId=" + appId + "&type=0";
+
+    $("#subord").click(function() {
+        $(".alert .weui-dialog__title").html("指派人员");
+        $(".alert").removeClass("hide");
     });
-/*branchUsers(token, appId); //党员支部信息展示
+    //弹框取消
+    $(".alert .default").click(function() {
+        $(".alert").addClass("hide");
+    });
+    //弹框确定
+    $(".alert .primary").click(function() {
+        $(".alert").addClass("hide");
+    });
+branchUsers(token, 10000000); //党员支部信息展示
 function branchUsers(token, appId) {
 	$.ajax({
 		type: "post",
@@ -79,17 +89,19 @@ function branchUsers(token, appId) {
 		url: "http://51service.xyz/Eagles/api/User/GetBranchUser",
 		dataType: "json",
 		success: function(res) {
-			$('#lp_w').html('');
+			$('#mys_zpy').html('');
 			var data = res.Result;
 			if(res.Code == 00) {
 				var options = '';
-				if(res.Result.BranchUsers != '' || res.Result.BranchUsers != null) {
+				if(res.Result.BranchUsers != '' && res.Result.BranchUsers != null) {
 					for(var i = 0; i < data.BranchUsers.length; i++) {
 						options += '<option value="' + data.BranchUsers[i].UserId + '">' + data.BranchUsers[i].UserName + '</option>'
 					}
-					$('#lp_w').append(options)
+					$('#mys_zpy').append(options)
+				}else{
+					$('.relation-list').html('无指派人员')
 				}
 			}
 		}
 	})
-}*/
+}
