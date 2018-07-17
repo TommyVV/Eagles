11564a657d86521477f78304262b169d335e1087
LAPTOP-RUV9P170\wukaihao <kaihao.wu@cisdi.com.cn>
2018-07-17 17:25:07 +0800

    积分设置接口调试

--- ManagementSite/fe-eagles-management/app/constants/ActionType/pc.js
+++ ManagementSite/fe-eagles-management/app/constants/ActionType/pc.js
@@ -38,6 +38,11 @@ const newsType = {
   SAVE_NEWS_INFO: "SAVE_NEWS_INFO", // 保存新闻详情
   CLEAR_NEWS_INFO: "CLEAR_NEWS_INFO" // 清空新闻详情
 };
+// 积分
+const scoreType = {
+  SAVE_INFO: "SAVE_INFO", // 保存详情
+  CLEAR_INFO: "CLEAR_INFO" // 清空详情
+};
 
 export const actionType = {
   appType,
@@ -47,5 +52,6 @@ export const actionType = {
   goodsType,
   programaType,
   orgType,
-  newsType
+  newsType,
+  scoreType
 };
--- ManagementSite/fe-eagles-management/app/constants/config/appconfig.js
+++ ManagementSite/fe-eagles-management/app/constants/config/appconfig.js
@@ -47,3 +47,49 @@ export const pageMap = [
 
 export const fileSize = 1024 * 1024 * 10;
 export const newsTempUrl = "http://51service.xyz/images/system/template.txt";
+export const scoreType = [
+  {
+    value: "0",
+    text: "发表文章奖励"
+  },
+  {
+    value: "1",
+    text: "文章字数奖励"
+  },
+  {
+    value: "2",
+    text: "文章关键字奖励"
+  },
+  {
+    value: "10",
+    text: "参加活动奖励"
+  },
+  {
+    value: "11",
+    text: "活动分享到支部奖励"
+  },
+  {
+    value: "12",
+    text: "活动分享到组织奖励"
+  },
+  {
+    value: "20",
+    text: "任务完成奖励"
+  },
+  {
+    value: "21",
+    text: "任务分享到支部奖励"
+  },
+  {
+    value: "22",
+    text: "任务分享到组织奖励"
+  },
+  {
+    value: "30",
+    text: "会议文章奖励"
+  },
+  {
+    value: "40",
+    text: "心得体会类型奖励"
+  }
+];
