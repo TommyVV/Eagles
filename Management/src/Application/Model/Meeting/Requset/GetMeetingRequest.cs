ce9af145d76f14234a938ed94aaf0ad36d9bfd15
TommyVV <644598674@qq.com>
2018-07-18 22:24:38 +0800

    add fields

--- Management/src/Application/Model/Meeting/Requset/GetMeetingRequest.cs
+++ Management/src/Application/Model/Meeting/Requset/GetMeetingRequest.cs
@@ -9,5 +9,10 @@
         /// 会议名称(可选参数）
         /// </summary>
         public string MeetingNmae { get; set; }
+
+        /// <summary>
+        /// 会议id，查询会议参与用户详情时，传递
+        /// </summary>
+        public int MeetingId { get; set; }
     }
 }
