e99be333260f51dd2538dcece8ced6ee69bad1f3
Administrator <Administrator@BLMRTPV28AMD2FY>
2018-07-20 02:45:05 +0800

    短信

--- Management/src/Interface/DataAccess/ISmsConfigDataAccess.cs
+++ Management/src/Interface/DataAccess/ISmsConfigDataAccess.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Eagles.Application.Model.SMS.Request;
+using Eagles.Application.Model.SMSOrg.Request;
 using Eagles.Base;
 using Eagles.DomainService.Model.Config;
+using Eagles.DomainService.Model.Org;
 
 namespace Eagles.Interface.DataAccess
 {
@@ -16,5 +18,11 @@ namespace Eagles.Interface.DataAccess
         int RemoveSMS(RemoveSMSRequset requset);
         List<SmsConfig> GetSMS(GetSMSRequset requset, out int totalCount);
         SmsConfig GetSMSDetail(GetSMSDetailRequset requset);
+        int EditOrgSmsConfig(TbOrgSmsConfig mod);
+        int CreateOrgSmsConfig(TbOrgSmsConfig mod);
+       
+        TbOrgSmsConfig GetSMSOrgDetail(GetSMSOrgDetailRequset requset);
+        List<TbOrgSmsConfig> GetSMSOrg(GetSMSOrgRequest requset, out int i);
+        int RemoveOrgSmsConfig(RemoveSMSOrgRequest requset);
     }
 }
