573253013df5e7fe1a9d6e1f63c46fc8fc2d2716
qixiaoer <1120637164@qq.com>
2018-07-21 10:16:21 +0800

    全局ajax验证添加

---
+++ site/js/compulicajax.js
@@ -0,0 +1,21 @@
+var appId = getRequest('appId');
+	$.ajaxSetup({
+	   contentType:"application/x-www-form-urlencoded;charset=utf-8",
+	   complete:function(XMLHttpRequest,textStatus){
+		  //通过XMLHttpRequest取得响应结果
+		  var rese = XMLHttpRequest.responseText;
+		  try{
+			var jsonData = JSON.parse(rese);
+			if(jsonData.Code == 11){
+			  //如果超时就处理 ，指定要跳转的页面(比如登陆页)
+			  alert(jsonData.Message);
+			  localStorage.clear();
+			  window.location.href = 'login.html?appId=' + appId;
+			  
+			}else{
+			  //正常情况就不统一处理了
+			}
+		  }catch(e){
+		  }
+		}
+	});
